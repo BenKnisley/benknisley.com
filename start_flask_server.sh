9fbbfacaba961a7c9bb9a19b9cb525678d5c5f12
BenKnisley <benknisley@gmail.com>
2020-07-28 10:59:49 -0400

    Added debug mode to start_flask_server

--- start_flask_server.sh
+++ start_flask_server.sh
@@ -2,8 +2,11 @@
 ## Flask Run File
 ## Author: Ben Knisley [benknisley@gmail.com]
 ## Date: March 1, 2020
+source ./venv/bin/activate
+
 export FLASK_APP=controller.py
 export FLASK_ENV=development
+export FLASK_DEBUG=1
 
-source ./venv/bin/activate
 python3 -m flask run
+
