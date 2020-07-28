959f63b093c6aeb825b5e885d062a67c71ac79f5
BenKnisley <benknisley@gmail.com>
2020-07-28 18:34:54 -0400

    Updated Tools

--- management/upload_page.py
+++ management/upload_page.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 
+
+with open('./management/auth_token.txt', 'r') as f:
+    auth_token = f.read()
+
 import requests
 
 url = 'https://benknisley.com/update_page/index'
 post_data = {
-    'auth': 'auth_token',
+    'auth': auth_token,
     'title': 'Home',
-    'data': '<hr>'
+    'data': """
+    <img src="profile.jpg" width="125" class="left">
+    <br>
+    <h1>Ben Knisley</h1>
+    <p>GIS Analyst at Crown Castle</p>
+    <p>Python programmer & cartographer</p>
+    <br>
+    <hr class="dashed" >
+    """
     }
     
 files = {
