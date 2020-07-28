31a86c3229d05ca8797a39d285414fc948a3acfc
BenKnisley <benknisley@gmail.com>
2020-07-28 16:10:06 -0400

    Setup Basic Function

--- controller.py
+++ controller.py
@@ -3,13 +3,79 @@
 Author: Ben Knisley [benknisley@gmail.com]
 Date: 27 July, 2020
 """
-from flask import Flask
+from flask import Flask, url_for, request, render_template
 app = Flask(__name__, static_folder="static")
 
+import model
 
 @app.route('/')
 def index():
-    return "Hello World"
+    title, html = model.get_page('index')
+    return render_template('page.html', title=title, html=html)
+
+@app.route('/resume')
+@app.route('/cv')
+def resume():
+    title, html = model.get_page('resume')
+    return render_template('page.html', title=title, html=html)
 
 
-Envirment
+@app.route('/blog')
+@app.route('/projects')
+def post_index():
+    title = "Blog Posts"
+
+    ##
+    posts = model.get_posts()
+
+    ## 
+    return render_template('blog_index.html', title=title, posts=posts)
+
+
+@app.route('/<post_id>')
+@app.route('/page/<post_id>', methods=['GET'])
+def get_post(post_id):
+    ## Get blog post contents
+    post_data = model.get_page(post_id)
+    
+    ## If post doesn't exist, call 404
+    if post_data == None:
+        return '404', 404
+
+    title, html = post_data
+
+    ## Returned rendered template
+    return render_template('page.html', title=title, html=html)
+
+
+@app.route('/add_page', methods=['POST'])
+@app.route('/page/<page_id>', methods=['POST'])
+def add_page(page_id=None):
+    """
+    Post method that adds a new page to database
+    """
+    print(page_id)
+    ## Confirm args exist
+    if 'auth' not in request.form:
+        return 'error'
+    if 'title' not in request.form:
+        return 'error'
+    if 'data' not in request.form:
+        return 'error'
+    
+    ## Check token
+    if not model.check_token(request.form['auth']):
+        return "Auth Error"
+
+    ## Add New page to DB
+    if page_id: ## If flagged edit existing post
+        model.update_page(page_id, request.form['title'], request.form['data'])
+    else: ## Not flagged add new post
+        page_id = model.add_page(request.form['title'], request.form['data'])
+
+    ## Save all posted files to static dir
+    files = request.files
+    for filename in files:
+        files[filename].save(f"./static/{filename}")
+
+    return page_id, 200
---
+++ model.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python3
+"""
+Author: Ben Knisley [benknisley@gmail.com]
+Date: 28 July, 2020
+"""
+import sys
+import sqlite3
+import time
+import random
+import hashlib, binascii
+import datetime
+
+db_path = "./database.db"
+
+def generate_pageid():
+    """
+    Generates a pageID
+    """
+    ## Create pool of chars and numbers to select from, abd lenght of ID
+    id_length = 7
+    chars = 'abcdefghijklmnopqrstuvwxyz'
+    digits = '0123456789'
+
+    ## Create rest of string
+    new_id = ''.join(random.choice(chars + digits) for _ in range(id_length))
+    
+    return new_id
+
+def generate_title_id(title):
+    """ """
+    ## Remove bordering whitespace
+    title_id = title.strip()
+
+    ## Make all chars lowercase
+    title_id = title_id.lower()
+
+    ## Replace lines with dashes
+    title_id = title_id.replace(' ', '-')
+
+    ## Return cleaned title_id
+    return title_id
+
+def add_page(title, html):
+    conn = sqlite3.connect(db_path)
+    sql = conn.cursor()
+
+    page_id = generate_pageid()
+    title_id = generate_title_id(title)
+    timestamp = int(time.time())
+
+    ## Setup query
+    query = "INSERT INTO pages VALUES (?, ?, ?, ?, ?)"
+    args = (page_id, title_id, timestamp, title, html)
+
+    ## Send query to database
+    sql.execute(query, args)
+
+    ## Commit changes to database
+    conn.commit()
+
+    return page_id
+
+def update_page(page_id, title, html):
+    conn = sqlite3.connect(db_path)
+    sql = conn.cursor()
+
+    title_id = generate_title_id(title)
+
+    ## Setup query
+    query = """
+        UPDATE pages
+        SET title_id = ?, title = ?, html = ?
+        WHERE page_id = ?; 
+    """
+    args = (title_id, title, html, page_id)
+
+    ## Send query to database
+    sql.execute(query, args)
+
+    ## Commit changes to database
+    conn.commit()
+
+def get_page(id):
+    ## Create database connection and cursor
+    conn = sqlite3.connect(db_path)
+    sql = conn.cursor()
+
+    ## Setup query
+    query = "SELECT title, html FROM pages WHERE page_id=? OR title_id=?;"
+    args = (id, id,)
+
+    ## execute query on database
+    sql.execute(query, args)
+
+    ## Get and return data from query
+    return sql.fetchone()
+
+def get_posts():
+    ## Create database connection and cursor
+    conn = sqlite3.connect(db_path)
+    sql = conn.cursor()
+
+    ## Setup query
+    query = "SELECT page_id, title, timestamp FROM pages;"
+    args = ()
+
+    ## execute query on database
+    sql.execute(query, args)
+    
+    pages = sql.fetchall()
+    posts = []
+
+    for page in pages:
+        ## Uppack page
+        page_id, title, timestamp = page
+        
+        ## Skip index and resume
+        if page_id in ('index', 'resume'):
+            continue
+
+        ## Create a list to store post data
+        post = []
+
+        ## Add page_id, and title
+        post.append(page[0])
+        post.append(page[1])
+
+        ## Add a human timestamp
+        timestr = f"{datetime.datetime.fromtimestamp(page[2]):%d %b, %Y}"
+        post.append(timestr)
+
+        ## Add post data to posts list
+        posts.append(post)
+
+    ## Get and return data from query
+    return posts
+
+
+##
+def generate_auth_token():
+    id_length = 50
+    chars = 'abcdefghijklmnopqrstuvwxyz'
+    digits = '0123456789'
+
+    ## Get one random char, can't be digit
+
+    ## Create rest of string
+    new_token = ''.join(random.choice(chars + digits) for _ in range(id_length))
+
+    ## Generate hash
+    hash_code = hashlib.pbkdf2_hmac('sha256', bytes(new_token, 'utf-8'), b'saltwater', 500000)
+    hash_code = str(binascii.hexlify(hash_code), 'utf-8')
+
+    ## Create database connection and cursor
+    conn = sqlite3.connect(db_path)
+    sql = conn.cursor()
+    
+    ## Setup query
+    query = "INSERT INTO auth_tokens VALUES (NULL, ?, ?)"
+    args = (int(time.time()), hash_code)
+
+    ## Send query to database, and commit
+    sql.execute(query, args)
+    conn.commit()
+
+    ## Return new_token
+    return new_token
+
+def check_token(token):
+    ## Generate hash
+    hash_code = hashlib.pbkdf2_hmac('sha256', bytes(token, 'utf-8'), b'saltwater', 500000)
+    hash_code = str(binascii.hexlify(hash_code), 'utf-8')
+
+    ## Create database connection and cursor
+    conn = sqlite3.connect(db_path)
+    sql = conn.cursor()
+    
+    ## Setup query
+    query = "SELECT id FROM auth_tokens WHERE hash=?;"
+    args = (hash_code,)
+
+    ## Send query to database
+    sql.execute(query, args)
+
+    hash_id = sql.fetchone()
+
+    if hash_id == None:
+        return False
+    else:
+        return True
